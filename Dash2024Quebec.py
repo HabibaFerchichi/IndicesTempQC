160f3625454d603bbcb1b5c8ab0de99fd674dd29
HabibaFerchichi <149753436+HabibaFerchichi@users.noreply.github.com>
2024-02-06 15:15:33 -0500

    Update Dash2024Quebec.py

--- Dash2024Quebec.py
+++ Dash2024Quebec.py
@@ -216,7 +216,9 @@ else:
     # Create bar chart of WQI (fixed and independent of other filters)
     fig = px.bar(df_selection, x='an', y='Valeur_Indice', color='Station', barmode='group',
                  width=600,height=650, template = "seaborn")
-fig.update_layout(xaxis=dict( tickvals = df_selection["an"].round(decimals=0).astype(int)))
+fig.update_layout(xaxis=dict(tickvals = df_selection["an"].astype(int)))
+
+
 
 
 ################################ Add new plot of Theorical Gaussian fitting using a,b,c parameters ######################
