792a4386dce4d63d7914b15400af938103354130
HabibaFerchichi <149753436+HabibaFerchichi@users.noreply.github.com>
2024-02-06 17:04:39 -0500

    Update Dash2024Quebec.py

--- Dash2024Quebec.py
+++ Dash2024Quebec.py
@@ -528,7 +528,7 @@ cols2=['moyenne', 'ecartype', 'min', '25%',
 cols3=['Riv', 'Station', 'an', 'Nb_ans', 'Nb_obs',
        'NB_obsEstivale']
 tab_stat[cols2]= tab_stat[cols2].round(decimals=2).astype(str)
-tab_stat[cols3]= tab_stat[cols3].astype(int)
+tab_stat[cols3]= tab_stat[cols3].round(decimals=0).astype(int)
 
 tab1 =pd.DataFrame()
 tab1 = tab_stat.query("Riv == @Riv & Station == @station & Variable== @varT")
