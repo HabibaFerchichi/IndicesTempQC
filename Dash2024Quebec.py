7026236d564bec35a680a39f2419824772d0c355
HabibaFerchichi <149753436+HabibaFerchichi@users.noreply.github.com>
2024-02-06 15:34:18 -0500

    Update Dash2024Quebec.py

--- Dash2024Quebec.py
+++ Dash2024Quebec.py
@@ -228,7 +228,7 @@ fig.update_layout(xaxis=dict(tickvals = df_selection["an"].astype(int)))
 #     "Sélectionner une année:",
 #     options = np.append('',x) ,
 #     default= x[0])
-an_selec = st.sidebar.selectbox("Proprietés du régime thermique: Sélectionner une année:", options=df_selection1 ["an"].unique(),default=df_selection1 ["an"].unique())
+an_selec = st.sidebar.selectbox("Proprietés du régime thermique: Sélectionner une année:", options=df_selection1 ["an"].unique())
 df_selection2 = df_selection1.query(
     "an == @an_selec")
 #extract data of gaussian parameters
