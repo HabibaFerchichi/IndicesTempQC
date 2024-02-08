310b393e251c8af78075fc0f1fee5547a2c30182
HabibaFerchichi <149753436+HabibaFerchichi@users.noreply.github.com>
2024-02-08 13:11:30 -0500

    Update Dash2024Quebec.py

--- Dash2024Quebec.py
+++ Dash2024Quebec.py
@@ -86,6 +86,9 @@ df= allindices_res1[['Riv', 'Station', 'an','Classe_Climatique','Indice',
        'longitude', 'latitude']]
 # change the name of quality index
 df['Definition']= df['Definition'].replace("Indice de qualité d'eau adapté","Indice composite de tolérance thermique")
+df['Definition']= df['Definition'].replace("Pic thermique:la moyenne interannuelle des températures moyennes des plus chauds 30 jours",
+                                           "Pic thermique:la moyenne annuelle des températures moyennes des plus chauds 30 jours)
+
 #cols=['an','Valeur_Indice', 'Nb_ans', 
 #       "Maximum annuelle de temperature d'air maximale (°C)",
 #       "Moyenne annuelle de temperature d'air maximale (°C)",
